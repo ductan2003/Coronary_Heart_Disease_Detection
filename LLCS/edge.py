d0226432023425b24ec341c0f71bb0e925180e08
ductan2003 <ductan10092003@gmail.com>
2024-12-22 01:25:58 +0700

    ECG: Final Update

--- LLCS/edge.py
+++
@@ -1,8 +0,0 @@
-class Edge:
-    def __init__(self, node, age = 1, weight=1):
-        self.node = node
-        self.weight = weight
-        self.age = age
-
-    def get_node(self):
-        return self.node
